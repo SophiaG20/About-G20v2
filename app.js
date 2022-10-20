912b18bc93f5d005c01ccd7a8f4c383ce9f3f2ef
Sophia G20 Grzeskowiak-Amezquita <SophiaG20@SophiaG20.com>
2022-10-19 20:09:54 -0700

    changes

---
+++ app.js
@@ -0,0 +1,15 @@
+let answer 1 = Do I go by G20 or Sophia?
+
+if (answer 1 === "G20") {
+    alert('G20 says Great job!');
+} else if 
+alert ('I go by G20');
+
+let answer 2 = Do I like Web3?
+
+if (answer 2 === "yes") {
+    alert('Web3 is the future!');
+} else if
+alert ('I love Web3');
+
+let answer 3 = Do I like to code?
